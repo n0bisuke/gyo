ff9883d37652e6bb73c2f5ac9b597dad034f450c
sugawara ryousuke <rsksound@gmail.com>
2014-09-20 23:34:47 +0900

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,52 @@
 //gulpの読み込み
 var gulp = require('gulp');
-//var config = require('./config.json');
 
 //プラグインの読み込み
 var $ = require('gulp-load-plugins')();
 var browser = require('browser-sync');
-// var browser = require('browser-sync');
 // var runSequence = require('run-sequence');
-
-// //pathsを記載
-// var BASE_PATH = 'app';
-// var BASE_PATH_SP ='app/sp';
-// var CSS_BASE_PATH = BASE_PATH + '/css';
-// var IMG_BASE_PATH = BASE_PATH + '/images';
-// var JS_BASE_PATH = BASE_PATH + '/js';
-// var DEST_PATH = 'dest';
-//var SERV_PATH = config.ftp.remotePath;
 
 //タスクの中身を記載
 var BASE_PATH = './';
 var ect_path = ['./template/*.ect'];
 var js_path = ['./js/*.js'];
-var css_path = ['./sass/*.scss'];
+var sass_path = ['./scss/*.scss'];
 
 //pc用
 //htmlに関するタスク
 gulp.task('html', function(){
     return gulp.src(ect_path)
-        // .pipe($.plumber())
+        .pipe($.plumber())
         .pipe($.ect())
-        // .pipe($.changed(BASE_PATH))
-        // .pipe($.htmlhint())
-        // .pipe($.htmlhint.reporter())
         .pipe($.prettify())
         .pipe(gulp.dest(BASE_PATH))
        .pipe(browser.reload({stream: true}));
 });
 
-gulp.task('style', function () {
-    return gulp.src(css_path)
+gulp.task('sass', function () {
+    gulp.src(sass_path)
+        .pipe($.plumber())
+        .pipe($.sass())
         .pipe($.autoprefixer(['last 3 version', 'ie >= 9', 'Android 4.0']))
-        .pipe(gulp.dest(BASE_PATH + 'css'))
+        .pipe(gulp.dest('./css'))
         .pipe(browser.reload({stream: true}));
 });
 
-// gulp.task('js', function(){
-//     return gulp.src(js_path)
-//         // .pipe($.plumber())
-//         .pipe($.ect())
-//         // .pipe($.changed(BASE_PATH))
-//         // .pipe($.htmlhint())
-//         // .pipe($.htmlhint.reporter())
-//         .pipe($.prettify())
-//         .pipe(gulp.dest(BASE_PATH))
-//        .pipe(browser.reload({stream: true}));
-// });
+gulp.task('js', function(){
+    return gulp.src(js_path)
+        .pipe($.plumber())
+        .pipe($.jshint())
+        .pipe($.prettify())
+        .pipe(gulp.dest(BASE_PATH+'js'))
+       .pipe(browser.reload({stream: true}));
+});
 
 //サーバー起動
 gulp.task('serv', function () {
     browser.init(null, {
         server: {
-            baseDir: './'
+            baseDir: './',
+            port: 6666
         }
     });
 });
@@ -72,11 +58,6 @@ gulp.task('reload', function () {
 
 gulp.task('default',['serv'],function(){
     gulp.watch(ect_path,['html']);
-    gulp.watch(css_path,['style']);
-    //gulp.watch(js_path,['js']);
-    // gulp.watch(path.pc.img.src,['image']);
-    // gulp.watch(path.sp.html.src_ect_watch,['html_sp']);
-    // gulp.watch(path.sp.sass.src,['style_sp']);
-    // gulp.watch(path.sp.js.src,['js_sp']);
-    // gulp.watch(path.sp.img.src,['image_sp']);
+    gulp.watch(sass_path,['sass']);
+    gulp.watch(js_path,['js']);
 });
