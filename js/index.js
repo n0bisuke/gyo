d3246b1f588767bce3789a68b48ec33f52428598
sugawara ryousuke <rsksound@gmail.com>
2014-09-20 22:59:51 +0900

    マージ

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 (function($){
     if(localStorage.gyo){
-    	
+        
     }else{//初アクセス
     	Gyo.init();
     }
 
-    $('#gyo').on('click', gyoPush);
+    console.log("YO!!!!");
+    $('.gyo').on('click', gyoPush);
     gyoDataStore.on("push", getGyo);
 
 })(jQuery);
 
 function gyoPush(){
-	var text = 'GYO';
-    	gyoDataStore.push({message : "aaa"},function(data){
-    		console.log("送信完了!");
-  	});
+    console.log("GYO!!!!");
+    var text = 'GYO';
+    
+    gyoDataStore.push({message : "aaa"},function(data){
+        console.log("送信完了!");
+    });
 }
 
 function getGyo(data){
